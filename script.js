e8dc4a14c61025515d971e110080b669cf284c80
bartgames <17bhattareyansh@ab-ed.org>
2026-02-17 15:48:53 +0000

    Remove comments from script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const overlay = document.getElementById("game-overlay");
 const frame = document.getElementById("game-frame");
 
-/* OPEN GAME IN FULL OVERLAY */
 function openGame(url) {
   frame.src = url;
   overlay.classList.remove("hidden");
 }
 
-/* CLOSE GAME */
 function closeGame() {
   frame.src = "";
   overlay.classList.add("hidden");
 }
 
-/* FULLSCREEN */
 function fullscreenGame() {
   if (frame.requestFullscreen) {
     frame.requestFullscreen();
   }
 }
 
-/* SEARCH */
 const searchInput = document.getElementById("search");
 
 searchInput.addEventListener("input", () => {
